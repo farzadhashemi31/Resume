92455c7357ed0c272fcc5d36c532e7e38d4594bb
Farzad <FarzadHashemi.work@gmail.com>
2025-09-06 17:04:07 +0330

    push

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const wraper  = document.getElementById("wraper")
+if(window.innerWidth < 1500){
+    wraper.classList.add("wraper")
+}
+ else 
+ {
+    wraper.classList.add("wraper_m")
+ }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "./public/**/*.{html,js,css}"
+  ],
+  theme: {
+    
+       screens:{
+    },
+    extend: {},
+  },
+  plugins: [],
+}
